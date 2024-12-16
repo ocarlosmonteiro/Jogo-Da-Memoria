a99e95caf97b48f38d2795de44db57e2fb633104
ocarlosmonteiro <carlos.monteiro@fatec.sp.gov.br>
2024-12-15 21:26:23 -0300

    Upload em novo repositorio

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 align = "center">JOGO DA MEMÓRIA</h1>
+
+<h2 align = "center">👨‍💻 OBJETIVO:</h2>
+<p>Desenvolver um jogo da memória como desafio proposto pela <u>DIO</u> no BootCamp -  Ri Happy - Front-end do Zero.</p>
+<p>Baseado em pokemon, o objetivo do jogo é encontrar os pokemons pares até que se encontre todos.</p>
+
+<h2 align = "center">👨‍💻 TECNOLOGIAS UTILIZADAS:</h2>
+
+![HTML5](https://img.shields.io/badge/HTML5-000000?style=for-the-badge&logo=html5&logoColor=orange)
+![CSS3](https://img.shields.io/badge/CSS3-000000?style=for-the-badge&logo=css3&logoColor=blue)
+![JavaScript](https://img.shields.io/badge/JavaScript-000000?style=for-the-badge&logo=javascript&logoColor=yellow)
+
+<h2 align = "center">📌 MELHORIAS:</h2>
+<ul>
+    <li>
+        <h3>Responsividade</h3>
+        <p>Nas próximas melhorias será incrementado a responsividade no jogo, para que o mesmo se adapte a diversas resoluções onde for reproduzido.</p>
+    </li>
+</ul>
---
+++ src/scripts/engine.js
@@ -0,0 +1,69 @@
+const pokemons = [
+    '<img src = "../src/images/bullbasaur.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/bullbasaur.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/charmander.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/charmander.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/dratini.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/dratini.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/eevee.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/eevee.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/pikachu.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/pikachu.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/psyduck.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/psyduck.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/snorlax.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/snorlax.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/squirtle.png" height="42" width="42" style="vertical-align: middle;">',
+    '<img src = "../src/images/squirtle.png" height="42" width="42" style="vertical-align: middle;">'
+]
+let openCards = []
+
+let shufflePokemons = pokemons.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < pokemons.length; i++)
+{
+    let box = document.createElement ("div");
+    box.className = "item";
+    box.innerHTML = shufflePokemons[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick()
+{
+    if(openCards.length < 2)
+    {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2)
+    {
+        setTimeout(checkMatch, 500);
+    }
+
+    console.log(openCards);
+}
+
+function checkMatch()
+{
+    if(openCards[0].innerHTML === openCards[1].innerHTML)
+    {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }
+    else
+    {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === pokemons.length)
+    {
+        alert("Você venceu!");
+        window.location.reload();
+    }
+}
+
